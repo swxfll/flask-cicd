9501da90beda0d03c65a558695f4a06e626d791e
swxfll <swxfll@outlook.com>
2023-04-02 15:16:34 +0800

    update

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return "<h1>" + str(datetime.datetime.now()) +  "<br>Version: V1.0.6</h1>"
+    return "<h1>" + str(datetime.datetime.now()) +  "<br>Version: V1.0.9</h1>"
 
 if __name__ == '__main__':
     app.run()
